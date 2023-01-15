1e62c3cf50d5d9c39f02576388de4cf6a82c6d28
ASHU8122004 <singhashishk12@gmail.com>
2023-01-15 21:01:04 +0530

    maths added

---
+++ math.js
@@ -0,0 +1,26 @@
+let x;
+
+let y = 15;
+
+let z = 59;
+
+let maximum;
+
+let minimum;
+
+//x = Math.round(x);
+
+//x Math.floor(x);
+
+//x = Math.ceil(x); //x Math.pow(x, 2);
+
+//x = Math.sqrt(x);
+
+//x Math.abs(x);
+
+//maximum = Math.max(x, y, z);
+
+//minimum = Math.min(x, y, z);
+
+x = Math.PI;
+Console.log(x);
