60ee6bc7dbba77aec61f353572af5536c9d82b39
ASHU8122004 <singhashishk12@gmail.com>
2023-01-15 16:11:09 +0530

    aritmetic added

---
+++ aritmetic.js
@@ -0,0 +1,20 @@
+/*arithmetic expression is a combination of... operands (values, variables, etc.)
+operators (+ - * / %)
+that can be evaluated to a value
+ex. y = x + 5;*/
+
+let students = 20;
+
+//students = students + 1;
+//students students 1;
+//students = students 2; //students students / 2;
+//let extraStudents students %
+//students 2;
+//students += 1
+//students--= 1;
+//students /= 2;
+
+//console.log(extraStudents);
+
+let result =(1 + 2) * (3 + 4);
+console.log(result);
