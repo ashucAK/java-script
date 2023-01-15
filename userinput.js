8f8f843db4964be3219a9e0938b7db964d98bfc6
ASHU8122004 <singhashishk12@gmail.com>
2023-01-15 16:57:42 +0530

    modified

--- userinput.js
+++ userinput.js
@@ -9,8 +9,10 @@
 let username;
 
 document.getElementById("myButton").onclick=function(){
-    username=document.getElementById("myText").value; 
+    
+    username = document.getElementById("myText").value; 
     console.log(username); 
+    
     document.getElementById("myLabel").innerHTML = "Hello", username;
 
 }
