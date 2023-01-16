20cfeedd6a22fac146c76ecfc7c5767269400436
ASHU8122004 <singhashishk12@gmail.com>
2023-01-16 21:11:55 +0530

    modified

--- random.js
+++ random.js
@@ -1,13 +1,11 @@
 let x;
-
 let y;
-
 let z;
 
 document.getElementById("rollButton").onclick= function(){
     x = Math.floor(Math.random() * 6) + 1; 
-    y= Math.floor(Math.random() * 6) + 1;
-    z= + 1;
+    y = Math.floor(Math.random() * 6) + 1;
+    z = Math.floor(Math.random() * 6) + 1;
 
 document.getElementById("xlabel").innerHTML = x; 
 document.getElementById("ylabel").innerHTML = y; 
