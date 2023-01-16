3dfd65aba774311b53d45befa507253c5577a9e5
ASHU8122004 <singhashishk12@gmail.com>
2023-01-16 21:42:25 +0530

    string method added

---
+++ stringme.js
@@ -0,0 +1,27 @@
+// useful string properties & methods
+
+let userName = "Bro Code";
+let phoneNumber = "123-456-7890";
+console.log(userName.length);
+
+console.log(userName.charAt(0)); 
+
+console.log(userName.indexOf("o"));
+
+console.log(userName.lastIndexOf("o")); 
+
+userName = userName.trim();
+
+console.log(userName);
+
+userName = userName.toUpperCase();
+
+console.log(userName);
+
+//userName = userName.toLowerCase();
+
+phoneNumber = phoneNumber.replaceAll("-", "/");
+
+console.log(phoneNumber);
+
+console.log(userName);
