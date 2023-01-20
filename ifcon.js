6829194c0ed4edff3a940b0385f3dfc35b407536
ASHU8122004 <singhashishk12@gmail.com>
2023-01-21 01:19:29 +0530

    if statemeny concept

---
+++ ifcon.js
@@ -0,0 +1,33 @@
+// if statement = if a condition is then do somethinng
+// if not then don't do anything
+// it's a form of decision making
+
+/*
+let a = 64;
+
+if(a >= 18 && a < 65){
+    console.log("You are now eligible to come in relationship");
+}
+else if(a >= 65){
+    console.log("You are going to die");
+}
+else if(a <= 18){
+    console.log("You are stil child dude");
+}
+else{
+    console.log("You are child just do whatever mama said")
+}
+*/
+
+//bollean condition
+
+let b = false;
+
+if(b){
+    console.log("You are now eligible to come in relationship");
+}
+else{
+    console.log("You are going to die");
+}
+
+
