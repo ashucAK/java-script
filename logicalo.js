fc2bece1ef05168fcd7ed08f4e2943ac15d4d9f1
ASHU8122004 <singhashishk12@gmail.com>
2023-01-22 22:37:56 +0530

    Added while do while

--- logicalo.js
+++ logicalo.js
@@ -4,7 +4,7 @@
 
 // || OR (Either condition can be true)
 
-
+/*
 let temp = 15;
 let sunny = false;
 
@@ -13,4 +13,56 @@ if(temp > 0 && temp< 30 && sunny){
 }
 else{
 console.log('The weather is bad!');
+}*/
+
+// NOT logical operator
+
+// typically used to reverse a condition's boolean value // true -> false false -> true
+
+/*
+let temp = 15;
+
+let sunny = false;
+
+if(!(temp> 0)){
+console.log("It's cold outside");
 }
+else{
+console.log("It's warm outside");
+}
+if(!sunny){
+console.log("It's cloudy outside");
+}
+else{
+console.log("It's sunny outside");
+}*/
+/*
+// while loop = repeat some code
+
+// while some condition is true
+
+// potentially infinite
+
+
+let userName = "";
+
+while(userName == "" || userName == null){
+    userName = window.prompt ("Enter your name");
+}
+console.log("Hello", userName);
+*/
+
+// do while loop = do something,
+
+// then check the condition, repeat if condition is true
+
+let userName;
+
+do{
+    userName = window.prompt("Enter Your name");
+}while(userName == "")
+console.log("YOO", userName);
+
+
+
+
