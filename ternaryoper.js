13b02895cbc1ca0908cc0bf2857517cb9bb33416
ASHISH KUMAR SINGH <singhashishk12@gmail.com>
2023-01-26 01:37:36 +0530

    added

---
+++ ternaryoper.js
@@ -0,0 +1,18 @@
+// ternary operator Shortcut for an if/else statement 
+// Takes 3 operands
+
+// condition ? exprIfTrue: exprIffalse
+
+//let adult = checkAge(21); console.log(adult);
+
+//function checkAge(age){
+
+//return age >= 18 ? true : false;
+
+//}
+
+checkWinner (false);
+
+function checkWinner (win){
+    win? console.log("YOU WIN!"): console.log("YOU LOSE");
+}
